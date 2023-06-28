55df7d88b3c8e8f332eca75e2a97ae0b1e7f868e
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-06-28 12:25:13 +0800

    Add firebase_core package and update dependencies to the latest versions

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:brew_crew/screens/wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
