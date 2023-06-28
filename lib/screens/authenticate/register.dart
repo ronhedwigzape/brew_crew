b96edb49e78a032850611e6e3515975158fea9d9
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-06-28 23:27:57 +0800

    Learn Register Email and Password
    - I can see what is registered in Firebase Authentication

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/foundation.dart';
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
-
   final Function toggleView;
   const Register({super.key, required this.toggleView});
 
@@ -11,64 +10,81 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // text field state
   String email = '';
   String password = '';
-
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: const Text('Sign up to Brew Crew'),
-        actions: <Widget>[
-          TextButton.icon(
-            icon: const Icon(Icons.person),
-            label: const Text('Sign in'),
-            onPressed: () => {
-              widget.toggleView(),
-            }
-          ),
-        ],
-      ),
-      body: Container(
-        padding:
-            const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          child: Column(children: <Widget>[
-            const SizedBox(height: 20.0),
-            TextFormField(onChanged: (val) {
-              setState(() => email = val);
-            }),
-            const SizedBox(height: 20.0),
-            TextFormField(
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() => password = val);
-                }),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.pink[400],
-              ),
-              onPressed: () async {
-                if (kDebugMode) {
-                  print('email: $email');
-                  print('password: $password');
-                }
-              },
-              child: const Text(
-                'Register',
-                style: TextStyle(color: Colors.white),
-              ),
-            )
-          ]),
-        )
-      )
-    );
+    return Scaffold(
+        backgroundColor: Colors.brown[100],
+        appBar: AppBar(
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          title: const Text('Sign up to Brew Crew'),
+          actions: <Widget>[
+            TextButton.icon(
+                icon: const Icon(Icons.person),
+                label: const Text('Sign in'),
+                onPressed: () => {
+                      widget.toggleView(),
+                    }),
+          ],
+        ),
+        body: Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+            child: Form(
+              key: _formKey,
+              child: Column(children: <Widget>[
+                const SizedBox(height: 20.0),
+                TextFormField(
+                    validator: (val) => val!.isEmpty ? 'Enter an email' : null,
+                    onChanged: (val) {
+                      setState(() => email = val);
+                    }),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                    obscureText: true,
+                    validator: (val) => val!.length < 6
+                        ? 'Enter a password 6+ chars long'
+                        : null,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    }),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pink[400],
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                      if (result == null) {
+                        setState(() {
+                          error = 'Please supply a valid email';
+                        });
+                      }
+                    }
+                  },
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 12.0),
+                Text(
+                  error,
+                  style: const TextStyle(
+                    color: Colors.red, 
+                    fontSize: 14.0
+                  ),
+                ),
+              ]),
+            )));
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -17,45 +17,49 @@ class AuthService {
     .map((User? user) => user != null ? _userFromFirebaseUser(user) : null);
   }
 
-
   // sign in anon
   Future signInAnon() async {
-
     try {
-
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
       if (result.user != null) {
         return _userFromFirebaseUser(user!);
       }
-
     } catch(e) {
-
       if (kDebugMode) {
         print(e.toString());
       }
       return null;
-
     }
-
   }
 
   // sign in with email & password
 
   // register with email & password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      if (result.user != null) {
+        return _userFromFirebaseUser(user!);
+      }
+    } catch(e) {
+      if (kDebugMode) {
+        print(e.toString());
+      }
+      return null;
+    }
+  }
 
   // sign out
   Future signOut() async {
     try {
-        
         return await _auth.signOut();
-  
-      } catch(e) {
-  
-        if (kDebugMode) {
-          print(e.toString());
-        }
-        return null;
+    } catch(e) {
+      if (kDebugMode) {
+        print(e.toString());
+      }
+      return null;
     }
   }
 }
