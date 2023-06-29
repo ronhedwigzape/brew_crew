f559c33db318589c0e8c764515ea27b42d67504d
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-06-29 18:12:12 +0800

    Create `signInWithEmailAndPassword()`
    - Make Sign in work

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -66,6 +66,13 @@ class _SignInState extends State<SignIn> {
               ),
               onPressed: () async {
                 if (_formKey.currentState!.validate()) {
+                  dynamic result = await _auth.signInWithEmailAndPassword(email, password);
+                  if (result == null) {
+                    setState(() {
+                      error = 'Could not sign in with those credentials';
+                    });
+                  }
+                  
                   if (kDebugMode) {
                     print('valid');
                   }
