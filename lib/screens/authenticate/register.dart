700656bc8a70e3cb1da7cef284b2d041aa2fed4f
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-06-29 19:54:35 +0800

    Learn to create text input decoration

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -42,12 +43,14 @@ class _RegisterState extends State<Register> {
           child: Column(children: <Widget>[
             const SizedBox(height: 20.0),
             TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Email'),
                 validator: (val) => val!.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 }),
             const SizedBox(height: 20.0),
             TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Password'),
                 obscureText: true,
                 validator: (val) => val!.length < 6
                     ? 'Enter a password 6+ chars long'
