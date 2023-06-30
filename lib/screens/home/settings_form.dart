7565ad783cbe5cd60d75f412afdca52f80aa9f76
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-06-30 18:56:47 +0800

    Learn to use Stream Builder

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,6 +1,10 @@
+import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   const SettingsForm({super.key});
@@ -33,70 +37,88 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          const Text(
-            'Update your brew settings.',
-            style: TextStyle(fontSize: 18.0),
-          ),
-          const SizedBox(height: 20.0),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: validateName,
-            onChanged: onNameChanged,
-          ),
-          const SizedBox(height: 20.0),
-          // dropdown
-          DropdownButtonFormField(
-            decoration: textInputDecoration,
-            value: _currentSugars ?? '0',
-            items: sugars.map((sugar) {
-              return DropdownMenuItem(
-                value: sugar,
-                child: Text('$sugar sugars'),
-              );
-            }).toList(),
-            onChanged: (value) {
-              setState(() {
-                _currentSugars = value;
-              });
-            }, 
-          ),
-          // slider
-          Slider(
-            min: 100,
-            max: 900,
-            divisions: 8,
-            label: _currentStrength?.toString() ?? '100',
-            activeColor: Colors.brown[_currentStrength ?? 100],
-            inactiveColor: Colors.brown[_currentStrength ?? 100],
-            value: (_currentStrength ?? 100).toDouble(), // .toDouble() converts int to double
-            onChanged: (val) => setState(() => _currentStrength = val.round()), // val.round() converts double to int
-          ),
-          // button
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.pink[400],
+
+    final user = Provider.of<FirebaseUser?>(context);
+
+    return StreamBuilder<UserData>(
+      stream: DatabaseService(uid: user?.uid).userData,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final userData = snapshot.data;
+          _currentName = userData?.name;
+          _currentSugars = userData?.sugars;
+          _currentStrength = userData?.strength;
+          return Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                const Text(
+                  'Update your brew settings.',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  initialValue: userData?.name,
+                  decoration: textInputDecoration,
+                  validator: validateName,
+                  onChanged: onNameChanged,
+                ),
+                const SizedBox(height: 20.0),
+                // dropdown
+                DropdownButtonFormField(
+                  decoration: textInputDecoration,
+                  value: _currentSugars ?? userData?.sugars,
+                  items: sugars.map((sugar) {
+                    return DropdownMenuItem(
+                      value: sugar,
+                      child: Text('$sugar sugars'),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _currentSugars = value;
+                    });
+                  }, 
+                ),
+                // slider
+                Slider(
+                  min: 100,
+                  max: 900,
+                  divisions: 8,
+                  label: _currentStrength?.toString() ?? '100',
+                  activeColor: Colors.brown[_currentStrength ?? userData!.strength!],
+                  inactiveColor: Colors.brown[_currentStrength ?? userData!.strength!],
+                  value: (_currentStrength ?? userData?.strength)!.toDouble(), // .toDouble() converts int to double
+                  onChanged: (val) => setState(() => _currentStrength = val.round()), // val.round() converts double to int
+                ),
+                // button
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pink[400],
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      if (kDebugMode) {
+                        print('valid');
+                        print(_currentName);
+                        print(_currentSugars);
+                        print(_currentStrength);
+                      }
+                    }
+                  },
+                  child: const Text(
+                    'Update',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ]
             ),
-            onPressed: () async {
-              if (_formKey.currentState!.validate()) {
-                if (kDebugMode) {
-                  print('valid');
-                  print(_currentName);
-                  print(_currentSugars);
-                  print(_currentStrength);
-                }
-              }
-            },
-            child: const Text(
-              'Update',
-              style: TextStyle(color: Colors.white),
-            ),
-          )
-        ]
-      ),
+          );
+        }
+        else {
+          return const Loading();
+        }
+      }
     );
   }
 }
